0780f4d7749a6e77f142e77a7ccdfd5c68f1d598
Steven <mitchs.patterson@gmail.com>
2016-09-15 11:42:00 -0600

    still working on blur

--- FlashCards/customDetailTableViewCell.swift
+++ FlashCards/customDetailTableViewCell.swift
@@ -14,16 +14,45 @@ class customDetailTableViewCell: UITableViewCell {
     
     @IBOutlet weak var answerLabel: UILabel!
     
+    var blurView: UIVisualEffectView?
+    
     override func awakeFromNib() {
         super.awakeFromNib()
-        // Initialization code
+        
     }
     
     
     func updateCardCells(card: Card, subject: Subject?) {
             self.questionLabel.text = card.question
             self.answerLabel.text = card.answer
+            blurLabel()
+            interactableLabel()
     }
-
+    
+    func blurLabel() {
+        let blur = UIBlurEffect(style: .Dark)
+        blurView = UIVisualEffectView(effect: blur)
+        
+        if let blurView = blurView {
+        blurView.frame = answerLabel.bounds
+        answerLabel.addSubview(blurView)
+        blurView.hidden = false }
+    }
+    
+    func interactableLabel() {
+        answerLabel.userInteractionEnabled = true
+        let gestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(handleTap))
+        answerLabel.addGestureRecognizer(gestureRecognizer)
+        
+    }
+    
+    func handleTap(sender: UITapGestureRecognizer) {
+        if sender.state == .Began {
+            blurView?.hidden = true
+        } else {
+            blurView?.hidden = false
+        }
+        
+    }
 
 }
